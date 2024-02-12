5f1f3cf9aecfe34ef89019f6005f12127d9a0553
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2024-02-12 23:25:36 +0000

    code updated

--- public_chat_backend/chatServer/index.js
+++ public_chat_backend/chatServer/index.js
@@ -1,19 +1,15 @@
-/**
- * This code sets up a Socket.IO server on port 8000 with CORS allowing all origins. 
- * It tracks online users, emits an 'userIncrement' event on connection, 
- * handles new user joining ('new-user-joined' event), and broadcasts messages ('send' event) 
- * along with handling likes ('liked' event). It also handles disconnections ('disconnect' event) 
- * by updating online user count and broadcasting 'disconnected' event.
-*/
+//Socket.IO server on port 8000 with CORS allowing all origins
 const io = require('socket.io')(8000, { cors: { origin: "*" } });
 
 const users = {};
 let onlineUsers = 0;
 
+//tracks online users, emits an 'userIncrement' event on connection
 io.on('connection', socket => {
     onlineUsers = onlineUsers + 1;
     socket.emit('userIncrement', onlineUsers);
 
+    //handles new user joining ('new-user-joined' event), and broadcasts messages ('send' event)
     socket.on('new-user-joined', name => {
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', { name: users[socket.id], onUsers: onlineUsers })
@@ -27,7 +23,9 @@ io.on('connection', socket => {
         socket.broadcast.emit('msg-like', id)
     })
 
+    //handles disconnections ('disconnect' event) 
     socket.on('disconnect', () => {
+        //updating online user count and broadcasting 'disconnected' event.
         onlineUsers = onlineUsers - 1;
         socket.broadcast.emit('disconnected', { name: users[socket.id], onUsers: onlineUsers })
         delete users[socket.id]
