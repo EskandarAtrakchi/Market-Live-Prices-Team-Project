4116b9d4b3e2f1e0743a713104c788ab7adcb759
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2024-02-12 23:18:54 +0000

    code updated

--- public_chat_backend/client.js
+++ public_chat_backend/client.js
@@ -28,12 +28,14 @@ if (name === null || name.length <= 3) {
     socket.emit('new-user-joined', name)
 }
 
-
+//listens for a 'userIncrement' event from the Socket.IO server and updates the inner text of an HTML element with the id 'onUsers' based on the received data.
 socket.on('userIncrement', data => {
     onUsers.innerText = data
 })
 
-const appendAction = (message, position) => {
+const appendAction = (message, position) => {//function `appendAction`
+    //new message element with a paragraph element containing the specified message and position
+    //appends it to the 'messageContainer' element, and ensures the container is scrolled to the bottom.
     const messageElement = document.createElement('div');
     const pElement = document.createElement('p');
     pElement.innerText = message;
@@ -44,6 +46,7 @@ const appendAction = (message, position) => {
     messageContainer.scrollTop = messageContainer.scrollHeight;
 }
 
+//appends a message element to the 'messageContainer' with a specified message, user, position, and unique id.
 const appendMessage = (message, user, position, id) => {
     const messageElement = document.createElement('div');
     const span = document.createElement('span');
@@ -60,30 +63,37 @@ const appendMessage = (message, user, position, id) => {
     messageElement.classList.add('message')
     messageElement.setAttribute('id', id)
     messageElement.setAttribute('ondblclick', "likedMessage(this.id)");
+
+    //The 'messageContainer' is then scrolled to the bottom.
     messageContainer.append(messageElement);
     messageContainer.scrollTop = messageContainer.scrollHeight;
 }
 
+//when user join
 socket.on('user-joined', data => {
     appendAction(`${data.name} Joined the Chat`, 'center')
     onUsers.innerText = data.onUsers
 })
 
+//when user send message to server send message to all user
 socket.on('receive',data =>{
     appendMessage(data.message,data.name,'left',data.id)
 })
 
+// if message is liked then implement the styling
 const likedMessage = (id)=>{
     const likedElement = document.getElementById(id);
     likedElement.classList.add('liked');
     socket.emit('liked',id)
 }
 
+// if message is liked then implement the styling
 socket.on('msg-like',id =>{
     const likedElement = document.getElementById(id);
     likedElement.classList.add('liked');
 })
 
+// when user disconnect from server then disconnect message should be shown
 socket.on('disconnected',data =>{
     appendAction(`${data.name} left the Chat`,'center')
     onUsers.innerHTML = data.onUsers
@@ -92,6 +102,7 @@ socket.on('disconnected',data =>{
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const message = messageInput.value;
+    //if message is nothing then return nothing
     if (message === "") {
         return
     }
