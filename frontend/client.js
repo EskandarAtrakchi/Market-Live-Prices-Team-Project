8fe139eeb83cfcbc6f3fd068b76dc34847d8c0c6
Alex <116267528+NCIAlexGar@users.noreply.github.com>
2024-04-09 19:49:37 +0100

    Update client.js

--- frontend/client.js
+++ frontend/client.js
@@ -1,5 +1,28 @@
-//establishing a Socket.IO connection to a server running at http://localhost:8000.
-const socket = io('http://localhost:8000');
+//importing io (input and output from socket server)
+import io from 'https://cdn.skypack.dev/socket.io-client';
+
+//establishing a Socket.IO connection to a server running at https://backend-chat-server.onrender.com
+//this server is running to connect with the backend that deployed on https://backend-chat-server.onrender.com
+
+const socket = io('https://backend-chat-server.onrender.com');
+//setting the three possibile scienarios 
+/**
+ * 1. if connection error from the server side
+ * 2. if the connection is timeout 
+ * 3. if the connection error from the server side 
+ */
+socket.on('connect_error', (err) => {
+    //message needs to be displayed 
+  console.log(`connect_error due to ${err.message}`);
+});
+
+socket.on('connect_timeout', (timeout) => {
+  console.log('connect_timeout:', timeout);
+});
+
+socket.on('error', (error) => {
+  console.log('error:', error);
+});
 
 //selecting various HTML elements (body, chatBox, onUsers, messageContainer, form, and messageInput) for manipulation in the document.
 const body = document.querySelector('.container');
@@ -12,8 +35,8 @@ const messageInput = document.getElementById('messageInput');
 //prompting the user to enter their name and stores it in the 'name' variable.
 const name = prompt('Enter your name to Join');
 
-//checking if the entered name is either null or has a length less than or equal to 3 characters. 
-if (name === null || name.length <= 3) {
+//checking if the entered name is either null or has a length less than or equal to 1 character. 
+if (name === null || name.length <= 1) {
     //it creates an h1 element with a message indicating denied access, removeing the chatBox from the document, appends the h1 element to the body
     const h1Element = document.createElement('h1');
     h1Element.classList.add('noAccess');
@@ -25,7 +48,7 @@ if (name === null || name.length <= 3) {
 } else {
     //'Access Granted' and emits a 'new-user-joined' event with the user's name using the established Socket.IO connection.
     alert('Access Granted');
-    socket.emit('new-user-joined', name)
+    socket.emit('new-user-joined', name)//add comments later 
 }
 
 //listens for a 'userIncrement' event from the Socket.IO server and updates the inner text of an HTML element with the id 'onUsers' based on the received data.
@@ -111,5 +134,3 @@ form.addEventListener('submit', (e) => {
     socket.emit('send', { message, id })
     messageInput.value = "";
 })
-
-
